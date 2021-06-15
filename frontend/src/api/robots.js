568509ae77118106b241902296a6dbcc049563ff
H-HChen <annhunter201@gmail.com>
2021-06-16 02:45:24 +0800

    Add IPv4 setting to bulk edit dialog

--- frontend/src/api/robots.js
+++ frontend/src/api/robots.js
@@ -1,4 +1,5 @@
 import request from '@/utils/request'
+import { Notification } from 'element-ui'
 
 export function fetchRobotList() {
   return request({
@@ -10,6 +11,14 @@ export function fetchRobotList() {
 export function setConfigDiff(data) {
   return request({
     url: '/robots/set_diff_config_by_id',
+    method: 'put',
+    data
+  })
+}
+
+export function setConfigSequential(data) {
+  return request({
+    url: '/robots/set_sequential_config_by_id',
     method: 'put',
     data
   })
@@ -44,4 +53,27 @@ export function fetchWifi() {
     url: '/robots/get_wifi_hotspot',
     method: 'get'
   })
+}
+
+export function responseVarify(response) {
+  for (const [agentID, configList] of Object.entries(response['data'])) {
+    for (const [configName, callbackValue] of Object.entries(configList)) {
+      if (callbackValue === '0') {
+        Notification({
+          title: 'Success',
+          message: 'Configuration Update Successfully',
+          type: 'success',
+          duration: 2000
+        })
+      } else {
+        Notification({
+          title: 'Error: Agent request failed',
+          dangerouslyUseHTMLString: true,
+          message: `Agent:<b>${agentID}</b>\n<b>${configName}</b> Set Failed`,
+          type: 'error',
+          duration: 5000
+        })
+      }
+    }
+  }
 }
--- frontend/src/components/BulkEdit/index.vue
+++ frontend/src/components/BulkEdit/index.vue
@@ -46,12 +46,36 @@
             </div>
           </el-form>
         </el-tab-pane>
+        <el-tab-pane label="IPv4" name="IPv4">IPv4 Address
+          <el-form label-position="left" label-width="90px" style="width: 90%; margin-left:50px; margin-top:20px">
+            <el-row>
+              <el-radio v-model="ipMethod" label="auto">Automatic (DHCP)</el-radio>
+              <el-radio v-model="ipMethod" label="manual">Manual</el-radio>
+              <el-checkbox v-model="ipSequential" :disabled="ipMethod=='auto'" border @change="TaskToFirst">Sequential IP</el-checkbox>
+            </el-row>
+            <div v-for="(ipArray, key) in agentIp" :key="key">
+              <el-row class="ip-header">
+                <el-col :span="4"><span>{{ key }}</span></el-col>
+              </el-row>
+              <el-row>
+                <el-input
+                  v-for="(segment, index) in ipArray"
+                  :key="index"
+                  v-model="ipArray[index]"
+                  maxlength="3"
+                  :disabled="ipMethod=='auto'"
+                  style="width: 10%"
+                />
+              </el-row>
+            </div>
+          </el-form>
+        </el-tab-pane>
       </el-tabs>
       <div slot="footer" class="dialog-footer">
         <el-button v-waves @click="closeDialog">
           Close
         </el-button>
-        <el-button v-waves :disabled="wifiSet.password.length<8" :loading="waitRequest" type="primary" @click="handleSubmit()">
+        <el-button v-waves :loading="waitRequest" type="primary" @click="handleSubmit()">
           Submit
         </el-button>
       </div>
@@ -61,7 +85,7 @@
 </template>
 
 <script>
-import { setConfigSame, setConfigDiff } from '@/api/robots'
+import { setConfigSame, setConfigDiff, setConfigSequential, responseVarify } from '@/api/robots'
 import waves from '@/directive/waves' // waves directive
 
 export default {
@@ -88,12 +112,20 @@ export default {
       waitRequest: false,
       sameAsAP: true,
       sameAsFirst: false,
+      ipSequential: true,
       currentTabName: 'Config',
-      AgentCurrentTasks: {}
+      AgentCurrentTasks: {},
+      agentIp: {
+        'IP Address': ['', '', '', ''],
+        'Subnet Mask': ['', '', '', ''],
+        'Gateway': ['', '', '', '']
+      },
+      ipMethod: 'auto'
     }
   },
   watch: {
     dialogShow(val) {
+      this.AgentCurrentTasks = Object.assign({}, {})
       if (val) {
         this.sameAsAP = true
         this.wifiSet = Object.assign({}, this.tempWifi)
@@ -106,6 +138,27 @@ export default {
     }
   },
   methods: {
+    checkAllProperty() {
+      if (!this.checkIpAddress('IP Address') || !this.checkIpAddress('Subnet Mask')) {
+        return false
+      } else if (!this.agentIp['Gateway'].every((element) => element === '') && !this.checkIpAddress('Gateway')) {
+        return false
+      }
+      var bitcode = this.agentIp['Subnet Mask'].reduce((total, current) => {
+        return total + Number(current).toString(2).padStart(8, '0')
+      }, '')
+
+      if (bitcode.split('01').length >= 2) {
+        return false
+      }
+      const prefix = bitcode.split('1').length - 1
+
+      return prefix
+    },
+    checkIpAddress(ipArray) {
+      var ipCheck = this.agentIp[ipArray].join('.')
+      return (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipCheck))
+    },
     TaskToFirst(val) {
       if (val) {
         var idList = Object.keys(this.AgentCurrentTasks)
@@ -141,12 +194,18 @@ export default {
       if (this.currentTabName === 'Config') {
         tempData = { 'device_list': Array.from(this.deviceList, device => device.DeviceID.toString(10)),
           'config_dict': { 'hostname': this.hostname }}
-        await setConfigSame(tempData)
+        setConfigSame(tempData).then(response => {
+          responseVarify(response)
+          this.waitRequest = false
+        })
         this.deviceList.forEach((element) => { element.Hostname = this.hostname })
       } else if (this.currentTabName === 'WiFi') {
         tempData = { 'device_list': Array.from(this.deviceList, device => device.DeviceID.toString(10)),
           'config_dict': { 'wifi': `${this.wifiSet.ssid} ${this.wifiSet.password}` }}
-        await setConfigSame(tempData)
+        setConfigSame(tempData).then(response => {
+          responseVarify(response)
+          this.waitRequest = false
+        })
         this.tempWifi['ssid'] = this.wifiSet.ssid
         this.tempWifi['password'] = this.wifiSet.password
         this.$emit('syncData')
@@ -155,18 +214,51 @@ export default {
         this.deviceList.forEach((element) => {
           tempData['device_config_json'][element.DeviceID] = { 'task_mode': this.AgentCurrentTasks[element.DeviceID] }
         })
-        await setConfigDiff(tempData)
+        setConfigDiff(tempData).then(response => {
+          responseVarify(response)
+          this.waitRequest = false
+        })
         this.deviceList.forEach((element) => {
           element['current_task'] = this.AgentCurrentTasks[element.DeviceID]
         })
+      } else if (this.currentTabName === 'IPv4') {
+        if (this.ipMethod === 'manual') {
+          const prefix = this.checkAllProperty()
+          if (!prefix) {
+            this.$message({
+              message: 'Invalid value of IPv4 Address',
+              type: 'warning'
+            })
+            this.waitRequest = false
+            return
+          }
+          tempData = { 'device_list': Array.from(this.deviceList, device => device.DeviceID.toString(10)) }
+          var configInput = `manual ${this.agentIp['IP Address'].join('.')} ${prefix}`
+          if (!this.agentIp['Gateway'].every((element) => element === '') && this.checkIpAddress('Gateway')) {
+            configInput = configInput + ' ' + this.agentIp['Gateway'].join('.')
+          }
+          if (this.ipSequential) {
+            tempData['numbering_config_start'] = { 'ip_address': configInput }
+            setConfigSequential(tempData).then(response => {
+              responseVarify(response)
+              this.waitRequest = false
+            })
+          } else {
+            tempData['config_dict'] = { 'ip_address': configInput }
+            setConfigSame(tempData).then(response => {
+              responseVarify(response)
+              this.waitRequest = false
+            })
+          }
+        } else if (this.ipMethod === 'auto') {
+          tempData = { 'device_list': Array.from(this.deviceList, device => device.DeviceID.toString(10)),
+            'config_dict': { 'ip_address': 'auto' }}
+          setConfigSame(tempData).then(response => {
+            responseVarify(response)
+            this.waitRequest = false
+          })
+        }
       }
-      this.waitRequest = false
-      this.$notify({
-        title: 'Success',
-        message: 'Configuration Update Successfully',
-        type: 'success',
-        duration: 2000
-      })
     }
   }
 }
@@ -183,5 +275,11 @@ export default {
   font-weight:bold;
   font-size:16px;
   margin-bottom: 20px;
+}
+.ip-header {
+  font-weight:bold;
+  font-size:16px;
+  margin-top: 20px;
+  margin-bottom: 10px;
 }
 </style>
