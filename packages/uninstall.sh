04c931a86d468f48d71dbd1f85bd502f150bc21d
ChenYing Kuo <evshary@gmail.com>
2021-06-16 09:20:50 +0800

    Update docker for backend.

--- packages/uninstall.sh
+++ packages/uninstall.sh
@@ -4,6 +4,7 @@ echo -n "Do you want to uninstall the RMT server? (y/N) "
 read answer
 if [ "$answer" '==' "y" ] || [ "$answer" '==' "Y" ]; then
     sudo docker rmi rmt-frontend
+    sudo docker rmi rmt-backend
 else
     echo "Do nothing"
 fi
