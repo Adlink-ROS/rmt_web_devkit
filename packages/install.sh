c3e1e1852c52ee425f45e1edb6f0beef55bb27a6
ChenYing Kuo <evshary@gmail.com>
2021-06-17 15:23:31 +0800

    Add version for rmt_webserver

--- packages/install.sh
+++ packages/install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-echo -n "Do you want to install the RMT server? (y/N) "
+version=`cat version.txt`
+echo -n "Do you want to install the RMT webserver ${version}? (y/N) "
 read answer
 if [ "$answer" '==' "y" ] || [ "$answer" '==' "Y" ]; then
     sudo apt install docker.io
-    sudo docker build -t rmt-frontend -f Dockerfile_frontend .
-    sudo docker build -t rmt-backend -f Dockerfile_backend .
+    sudo docker build -t rmt-frontend:${version} -f Dockerfile_frontend .
+    sudo docker build -t rmt-backend:${version} -f Dockerfile_backend .
 else
     echo "Do nothing"
 fi
--- packages/rmt_server.sh
+++ packages/rmt_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+version=`cat version.txt`
 if [ "$1" = "stop" ]; then
     echo "Stop the RMT server"
     sudo docker stop -t 1 rmt-frontend-container
@@ -7,9 +8,9 @@ if [ "$1" = "stop" ]; then
 elif [ "$1" = "start" ]; then
     echo "Start the RMT server"
     # Use port 9527
-    sudo docker run -d -p 9527:9527 --rm --name rmt-frontend-container rmt-frontend
+    sudo docker run -d -p 9527:9527 --rm --name rmt-frontend-container rmt-frontend:${version}
     # Use port 8080
-    sudo docker run -d --privileged --volume /var/run/dbus:/var/run/dbus --network=host --rm --name rmt-backend-container rmt-backend
+    sudo docker run -d --privileged --volume /var/run/dbus:/var/run/dbus --network=host --rm --name rmt-backend-container rmt-backend:${version}
 else
     echo "Usage: ./rmt_server.sh [command]"
     echo "command:"
