4331d8ad70655cc6b02895909dc3c84827104e30
ChenYing Kuo <evshary@gmail.com>
2021-06-29 14:07:13 +0800

    Able to get docker images from docker hub.

--- packages/install.sh
+++ packages/install.sh
@@ -5,10 +5,12 @@ echo -n "Do you want to install the RMT webserver ${version}? (y/N) "
 read answer
 if [ "$answer" '==' "y" ] || [ "$answer" '==' "Y" ]; then
     sudo apt install docker.io
-    sudo docker build -t rmt-frontend:${version} -f Dockerfile_frontend .
-    sudo docker build -t rmt-backend:${version} -f Dockerfile_backend .
+    sudo docker pull adlinkrmt/rmt-frontend:${version}
+    sudo docker pull adlinkrmt/rmt-backend:${version}
     sudo cp rmt-server /usr/bin/rmt-server
     sudo sed -i "s/vx.x.x/${version}/g" /usr/bin/rmt-server
+    sudo sed -i "s/my_frontend_name/adlinkrmt\/rmt-frontend/g" /usr/bin/rmt-server
+    sudo sed -i "s/my_backend_name/adlinkrmt\/rmt-backend/g" /usr/bin/rmt-server
 else
     echo "Do nothing"
 fi
