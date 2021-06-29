029d78bc68f12ad32b3e1d7ee0f1ae100ea03308
ChenYing Kuo <evshary@gmail.com>
2021-06-29 11:00:22 +0800

    Support remove in rmt-server.

--- packages/install.sh
+++ packages/install.sh
@@ -7,6 +7,8 @@ if [ "$answer" '==' "y" ] || [ "$answer" '==' "Y" ]; then
     sudo apt install docker.io
     sudo docker build -t rmt-frontend:${version} -f Dockerfile_frontend .
     sudo docker build -t rmt-backend:${version} -f Dockerfile_backend .
+    sudo cp rmt-server /usr/bin/rmt-server
+    sudo sed -i "s/vx.x.x/${version}/g" /usr/bin/rmt-server
 else
     echo "Do nothing"
 fi
